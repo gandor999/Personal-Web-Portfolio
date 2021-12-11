b531f332a65465d6819292489f3483f3f8585e7c
gandor999 <geodorruales@gmail.com>
2021-12-11 17:25:18 +0800

    Add project contents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,23 +7,22 @@ export default function Home() {
   return (
 
       <div className="font-link">
-        <header className="page-cloud-bg hello justify-content-center flex-column text-center p-2">
-          <section className="d-flex justify-content-center mb-4 p-3">
-            <div className="pr-4">
+        <header className="page-cloud-bg hello justify-content-center flex-column text-center p-2 container-fluid">
+          <section className="d-flex justify-content-center mb-4 p-3 flex-column welcome-section">
+            <div id="welcome-wrapper" className="rounded-pill container-fluid border border-2 border-dark">
               <h1 className="welcome">
-                Welcome
+                <strong>Welcome</strong>
               </h1>
             </div>
-            <div className="">
-              <p className="">
+            <div id="" className="pt-4">
+              <p className="home-text">
                 To Geodor's web portfolio
               </p>
             </div>
           </section>
           
-          <section className="mt-4">
-            <p>Take a look at the stuff he's done <Link to="/projects">here</Link></p>
-            <p>or about <Link to="/aboutme">him</Link></p>
+          <section className="mt-4 home-text p-4">
+            <p>Take a look at the stuff he's done <Link to="/projects" className="text-dark">here</Link> or about <Link to="/aboutme" className="text-dark">him</Link></p>
           </section>
         </header>  
       </div>
@@ -31,4 +30,6 @@ export default function Home() {
     
   );
 }
+
+
 
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,81 @@
-
+import { Row, Col, Container, Card, Button } from 'react-bootstrap';
+import responsiveWeb from '../images/responsiveWeb.png';
+import eCommerceAPI from '../images/eCommerceAPI.png';
+import eCommerceReact from '../images/eCommerceReact.png';
+import TicTacToe from '../images/TicTacToe.png';
 
 export default function Projects(){
 	return (
-		<div className="page-coconut-bg">
-			Under construction
-		</div>
+		<Container fluid className="page-coconut-bg flex-column align-items-center">
+
+			<section className="">
+				<div className="projects-header-wrapper mt-5 rounded-pill border border-2 border-dark justify-content-center">
+					<h1 className=" text-center p-1">
+						Projects
+					</h1>	
+				</div>
+				
+			</section>
+
+			<section className="justify-content-center container-fluid p-4">
+					<Row>
+						<Col md={6} className="p-3 d-flex justify-content-center">
+							<Card className="card-width h-100 p-1">
+							  <Card.Img variant="top" src={responsiveWeb} />
+							  <Card.Body className="d-flex flex-column">
+							    <Card.Title className="text-center">Static Responsive Website</Card.Title>
+							    <Card.Text>
+							      This is the first website project I made with Bootstrap, CSS, and HTML.
+							    </Card.Text>
+							    <Button variant="primary" className=" mt-auto">Take a look!</Button>
+							  </Card.Body>
+							</Card>
+						</Col>
+
+						<Col md={6} className="p-3 d-flex justify-content-center">
+							<Card className="card-width h-100 p-1">
+							  <Card.Img variant="top" src={eCommerceAPI} />
+							  <Card.Body className="d-flex flex-column">
+							    <Card.Title className="text-center">eCommerce API</Card.Title>
+							    <Card.Text>
+							      This API was made using Node, Express, and Mongoose. The database used was MongoDB Atlas so this is a NoSql DB. 
+							    </Card.Text>
+							    <Button variant="primary" className=" mt-auto">Read the code</Button>
+							  </Card.Body>
+							</Card>
+						</Col>
+
+						<Col md={6} className="p-3 d-flex justify-content-center">
+							<Card className="card-width h-100 p-1">
+							  <Card.Img variant="top" src={eCommerceReact} />
+							  <Card.Body className="d-flex flex-column">
+							    <Card.Title className="text-center">eCommerce Simulation Website</Card.Title>
+							    <Card.Text>
+							      This is the first project that I made with React. It has CRUD operations with the admin account and CR operations for a normal user.
+
+							      It simulates a simple eCommerce website.
+							    </Card.Text>
+							    <Button variant="primary" className=" mt-auto">Add to Cart!</Button>
+							  </Card.Body>
+							</Card>
+						</Col>
+
+						<Col md={6} className="p-3 d-flex justify-content-center">
+							<Card className="card-width h-100 p-1">
+							  <Card.Img variant="top" src={TicTacToe} />
+							  <Card.Body className="d-flex flex-column">
+							    <Card.Title className="text-center">Tic Tac Toe</Card.Title>
+							    <Card.Text>
+							      This is a tic tac toe game made with React.
+							    </Card.Text>
+							    <Button variant="primary" className=" mt-auto">Play!</Button>
+							  </Card.Body>
+							</Card>
+						</Col>
+					</Row>
+			</section>
+			
+		</Container>
+		
 	)
 }
